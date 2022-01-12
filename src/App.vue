aafd96c9345ae3b8d22733ac22eafab244a25b95
Edgarlct <90525103+Edgarlct@users.noreply.github.com>
2022-01-12 19:18:01 +0100

    :construction: add NavMenu (nav bar, sidebar social and scroll)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+<NavMenu></NavMenu>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import NavMenu from "./components/NavMenu";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    NavMenu,
   },
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "styles/reset";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  background:radial-gradient(circle at 30% 5%, rgb(84, 84, 84) 0%, rgb(49, 49, 49) 30%, rgb(8, 8, 8) 100%);
+  background:-moz-radial-gradient(circle at 30% 5%, rgb(84, 84, 84) 0%, rgb(49, 49, 49) 30%, rgb(8, 8, 8) 100%);
+  background:-webkit-radial-gradient(circle at 30% 5%, rgb(84, 84, 84) 0%, rgb(49, 49, 49) 30%, rgb(8, 8, 8) 100%);
+  background:-o-radial-gradient(circle at 30% 5%, rgb(84, 84, 84) 0%, rgb(49, 49, 49) 30%, rgb(8, 8, 8) 100%);
+  background:-ms-radial-gradient(circle at 30% 5%, rgb(84, 84, 84) 0%, rgb(49, 49, 49) 30%, rgb(8, 8, 8) 100%);
 }
 </style>
---
+++ src/components/NavMenu.vue
@@ -0,0 +1,129 @@
+<template>
+<div class="container-menu">
+  <div class="sibebar-left">
+    <p class="logo">ed</p>
+    <ul class="container-reseaux">
+      <li><a href="#"><i class="fab fa-github"></i></a></li>
+      <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+    </ul>
+  </div>
+  <div class="sibebar-right">
+    <nav>
+      <ul>
+        <li><a href="#">Présentation</a></li>
+        <li><a href="#">Compétence</a></li>
+        <li><a href="#">Projet</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="container-scroll">
+      <span></span>
+      <div>
+        <p>scroll</p>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "NavMenu"
+}
+</script>
+
+<style lang="scss" scoped>
+  .container-menu{
+    position: fixed;
+    padding: 2rem;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+    >div{
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+
+    i{
+      color: $gray-600;
+      font-size: 1.5rem;
+    }
+  }
+
+  .sibebar-left{
+    align-items: center;
+  }
+
+  .sibebar-right{
+    align-items: flex-end;
+  }
+
+  .container-reseaux{
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .logo{
+    text-transform: uppercase;
+    font-size: 2rem;
+    font-weight: 900;
+    color: $gray-200;
+  }
+
+  nav{
+    ul{
+      display: flex;
+      gap: 2rem;
+
+      a{
+        text-decoration: none;
+        color: $gray-200;
+      }
+    }
+  }
+
+  .container-scroll{
+    height: 100px;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    width: 20px;
+
+    span{
+      position: absolute;
+      transform: translate(-50%,0%);
+      left: 50%;
+      display: block;
+      height: 100%;
+      width: 2px;
+      background-color: $gray-400;
+    }
+
+    div{
+      position: relative;
+      top: 0px;
+      background-color: rgb(8, 8, 8);
+      z-index: 1;
+      height: fit-content;
+      width: fit-content;
+      width: -moz-fit-content;
+      display: flex;
+      align-items: center;
+      width: 100%;
+    }
+
+    p{
+      text-transform: uppercase;
+      font-size: 1rem;
+      writing-mode: vertical-lr;
+      text-orientation: mixed;
+      padding: .5rem 0;
+      color: $gray-400;
+    }
+  }
+
+</style>
