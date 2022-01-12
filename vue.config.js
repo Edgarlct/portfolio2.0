a3d0edb6047a62a3000df68c939035c4ae216131
Edgarlct <90525103+Edgarlct@users.noreply.github.com>
2022-01-12 14:14:02 +0100

    :wrench: update config files for use scss

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+    chainWebpack: config => {
+        const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+        types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
+    },
+    css: {
+        loaderOptions: {
+            scss: {
+                additionalData: `@import "~@/styles/variables.scss";`
+            },
+        }
+    }
+}
+
+function addStyleResource (rule) {
+    rule.use('style-resource')
+        .loader('style-resources-loader')
+        .options({
+            patterns: [
+                path.resolve(__dirname, './src/styles/imports.styl'),
+            ],
+        })
+}
