ab8dcb6c58c8da813c3cf8e6b99b266a8d1d2d8e
Edgarlct <90525103+Edgarlct@users.noreply.github.com>
2022-01-14 02:49:24 +0100

    :sparkles: add dinamic project list

--- src/components/ListProjects.vue
+++ src/components/ListProjects.vue
@@ -1,114 +1,121 @@
 <template>
   <div class="container-list-projet">
     <div>
-      <div class="image-project"><img :src="projet[0].img" alt=""></div>
-      <div class="firstProject"><img :src="projet[1].img" alt=""></div>
-      <div class="image-project"><img :src="projet[2].img" alt=""></div>
+      <div class="image-project"><img :src="displayImg(-1)" alt=""></div>
+      <div class="firstProject"><img :src="displayImg(0)" alt=""></div>
+      <div class="image-project"><img :src="displayImg(1)" alt=""></div>
       <div class="detail">
-        <h2>{{projet[1].name}}</h2>
-        <p>{{projet[1].description}}</p>
-        <div class="btn-see-plus">Voir le projet</div>
+        <h2>{{ displayName() }}</h2>
+        <p>{{ displayDesc() }}</p>
+        <div class="btn-nav">
+          <div class="btn-arrow" @click="before()"><i class="fas fa-arrow-left"></i>Précédent</div>
+          <div class="btn-see-plus">Voir le projet</div>
+          <div class="btn-arrow" @click="next()">Suivant<i class="fas fa-arrow-right"></i></div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import changeProject from "../use/use-ListProject"
+
 export default {
   name: "ListProjects",
-  data(){
-    return{
-      projet: {
-        0:{
-          img:require('../assets/spidermanHome.jpg'),
-          name: 'Spider-man',
-          description: 'Projet réalisé pour la marque Monade, fabricant et distributeur de limonades Bio. Le site a été entièrement designé et développé en étroite collaboration avec le responsable de la marque.'
-        },
-        1:{
-          img:require('../assets/monadeHome.jpg'),
-          name: 'Monade Bio',
-          description: 'Projet réalisé pour valider ma première année. Il s\'agissait de réaliser un site web sur un personnage de fiction, avec une gestion de blog et d’utilisateur.'
-        },
-        2:{
-          img:require('../assets/app.jpg'),
-          name: 'Association du pôle LDV',
-          description: 'Dans le cadre de ma formation en deuxième année, en groupe nous avons pour objectif de réaliser une application cross-plateforme, pour les associations présentes sur le campus.'
-        }
-      }
-    }
+  setup() {
+    const {displayImg, displayName, displayDesc, next, before} = changeProject();
+    return {displayImg, displayName, displayDesc, next, before}
+  },
+  data() {
+    return {}
   }
 }
 </script>
 
-<style lang="scss" scoped>
-  .container-list-projet{
-    height: 100vh;
+<style lang="scss">
+.container-list-projet {
+  height: 100vh;
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+
+  > div {
+    height: 60%;
     display: flex;
+    align-items: end;
     position: relative;
-    flex-direction: column;
     justify-content: center;
+    transform: translate(0%, -10%);
 
-    >div{
-      height: 60%;
-      display: flex;
-      align-items: end;
-      position: relative;
-      justify-content: center;
-      transform: translate(0%, -10%);
+    > div {
+      @include flex-center;
+    }
 
-      >div{
-        @include flex-center;
-      }
+    .firstProject {
+      height: 100%;
+      width: 40%;
+      overflow: hidden;
+      margin: 0 1rem;
+    }
 
-      .firstProject{
-        height: 100%;
-        width: 40%;
-        overflow: hidden;
-        margin: 0 1rem;
-      }
-      .image-project{
-        height: 85%;
-        width: 30%;
-        overflow: hidden;
-        filter: blur(5px);
-      }
+    .image-project {
+      height: 85%;
+      width: 30%;
+      overflow: hidden;
+      filter: blur(5px);
+    }
 
-      img{
-        height: 100%;
-        @include black-and-white;
-      }
+    img {
+      height: 100%;
+      @include black-and-white;
     }
   }
+}
 
 
+.detail {
+  position: absolute;
+  width: 60%;
+  transform: translate(0, 100%);
+  bottom: 15%;
 
-  .detail{
-    position: absolute;
-    width: 60%;
-    transform: translate(0, 100%);
-    bottom: 15%;
+  flex-direction: column;
 
-    flex-direction: column;
+  background-color: $gray-800;
+  padding: 1rem 1.5rem;
 
-    background-color: $gray-800;
-    padding: 1rem 1.5rem;
+  h2 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+  }
 
-    h2{
-      font-size: 2rem;
-      margin-bottom: 1rem;
-    }
+  p {
+    line-height: 1.75rem;
+    margin-bottom: 1.5rem;
+  }
 
-    p{
-      line-height: 1.75rem;
-      margin-bottom: 1.5rem;
-    }
+  .btn-nav {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
 
-    .btn-see-plus{
-      padding: 1rem 1.5rem;
-      background-color: $black;
+    .btn-arrow {
+      color: $gray-300;
+      display: flex;
+      align-items: center;
+      gap: .5rem;
       cursor: pointer;
     }
-
   }
+
+  .btn-see-plus {
+    padding: 1rem 1.5rem;
+    background-color: $black;
+    cursor: pointer;
+  }
+
+}
 
 </style>
---
+++ src/use/use-ListProject.vue
@@ -0,0 +1,68 @@
+<script>
+import { ref} from "vue";
+const project = [
+  {
+    img:require('../assets/spidermanHome.jpg'),
+    name: 'Spider-man',
+    description: 'Projet réalisé pour la marque Monade, fabricant et distributeur de limonades Bio. Le site a été entièrement designé et développé en étroite collaboration avec le responsable de la marque.'
+  },
+  {
+    img:require('../assets/monadeHome.jpg'),
+    name: 'Monade Bio',
+    description: 'Projet réalisé pour valider ma première année. Il s\'agissait de réaliser un site web sur un personnage de fiction, avec une gestion de blog et d’utilisateur.'
+  },
+  {
+    img:require('../assets/app.jpg'),
+    name: 'Association du pôle LDV',
+    description: 'Dans le cadre de ma formation en deuxième année, en groupe nous avons pour objectif de réaliser une application cross-plateforme, pour les associations présentes sur le campus.'
+  }
+]
+export default function changeProject(){
+  const len = project.length;
+  let currentValue = ref(1);
+  function next(){
+    if (currentValue.value == len-1){
+      currentValue.value = 0
+    }
+    else {
+      currentValue.value++;
+    }
+    return currentValue.value;
+  }
+  function before(){
+    if (currentValue.value == 0){
+      currentValue.value = len-1;
+    }
+    else {
+      currentValue.value--;
+    }
+    return currentValue.value;
+  }
+
+  function displayImg(i){
+    if (currentValue.value + i == -1){
+      return project[len-1].img
+    }
+
+    else if (currentValue.value + i == len){
+      return project[0].img
+    }
+    else {
+      console.log(currentValue.value + i )
+      return project[currentValue.value+i].img
+    }
+  }
+  function displayName(){
+    let name = ref(project[currentValue.value].name)
+    return name.value
+  }
+  function displayDesc(){
+    let desc = ref(project[currentValue.value].description)
+    return desc.value
+  }
+
+  return {displayImg, displayName, displayDesc, next, before};
+
+}
+
+</script>
