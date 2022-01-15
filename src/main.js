541521dfae303c513e43f78b3180c01475720c66
Edgarlct <90525103+Edgarlct@users.noreply.github.com>
2022-01-15 22:44:51 +0100

    :wrench: add link for new package

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import veProgress from 'vue-ellipse-progress';
 
 const app = createApp(App);
 app.use(router)
+app.use(veProgress)
 app.mount("#app");
